0bd476589dfda3eda60a8cbbe37c63560c177a37
Christian Angulo <christiananguloprogramador23@gmail.com>
2021-06-22 22:22:40 -0400

    subida

--- src/main/java/cl/christian/demo/controler/Controlador.java
+++ src/main/java/cl/christian/demo/controler/Controlador.java
@@ -31,14 +31,7 @@ import cl.christian.demo.modelo.CartelPublicitario;
  * @author Christian
  *
  */
-/**
- * @author Christian
- *
- */
-/**
- * @author Christian
- *
- */
+
 @Controller
 @RequestMapping
 public class Controlador {
@@ -46,12 +39,21 @@ public class Controlador {
 	@Autowired
 	private ICampaniaPublicitariaService service;
 	
+	/**
+	 * carga los datos de la bd en una lista y luego los manda 
+	 * a una vista 
+	 */
 	@GetMapping("/listar")
 	public String listarcampania(Model model) {
 		List<CampaniaPublicitaria>campaniapublicitaria=service.listarcampania();
 		model.addAttribute("campaniapublicitaria", campaniapublicitaria);
 		return "index";
 	}
+	
+	/**
+	 * carga los datos de la bd en una lista y luego los manda 
+	 * a una vista 
+	 */
 	@GetMapping("/listarPublicidad")
 	public String listarcampaniaPublidad(Model model) {
 		List<CampaniaPublicitaria>campaniapublicitaria=service.listarcampania();
@@ -62,14 +64,26 @@ public class Controlador {
 	
 	
 	
+	/**
+	 * muestra la vista formulario agregar campaña
+	 * cuando lo llamn por "/new"
+	 */
 	@GetMapping("/new")
-	//llama
 	public String agregarcampania(Model model) {
 		model.addAttribute("campaniapublicitaria", new CampaniaPublicitaria());
 		return "usuario/FormularioAgregarCampaña";
 	}
 	
 	
+	
+	/**
+	 * @param c
+	 * @param model
+	 *Guarda los datos del formulario campaña  y la imagen
+	 * los guarda en la base de dato
+	 * @param attributes
+	 * @return
+	 */
 	@PostMapping("/savecampania")
 	public String savecampania(@Validated CampaniaPublicitaria c, Model model, @RequestParam("file") MultipartFile image, RedirectAttributes attributes) {
 		
@@ -92,17 +106,23 @@ public class Controlador {
 			}
 		}
 		
-		
+		model.addAttribute("titulo","Agregar  Campania-Empresa");
 	
 		service.savecampania(c);
 		return "redirect:/listar";	
 	}
 	
+	/**
+	 * primero busca en la lista el id que le mandamos atraves de un boton
+	 * y luego manda los datos para cargalos
+	 * para luego poder modificarlo llamando a la clase save de campaña
+	 */
 	@GetMapping("modificarcampania/{id}")
 	public String editar(@PathVariable("id") int id,Model model) {
 		
 		CampaniaPublicitaria campaniapublicitaria=service.listarId(id);
 		
+		model.addAttribute("titulo","Modificar Campania-Empresa");
 		model.addAttribute("campaniapublicitaria",campaniapublicitaria);
 		
 		return"usuario/FormularioAgregarCampaña";
@@ -110,11 +130,18 @@ public class Controlador {
 	
 	
 	/*Detalle de campaña por id*/
+	
+	/**
+	 * Buscar el id en la lista
+	 * Buscar si existe o no 
+	 * Luego de encontrarlo lo manda a una vista que detalla la campaña y sus datos
+	 * 
+	 */
 	@GetMapping("detalle/{id}")
 	public String detalleCampania(@PathVariable("id") int id,Model model,RedirectAttributes attribute) {
 		
 		 CampaniaPublicitaria campaniapublicitaria= null;
-		//CampaniaPublicitaria campaniapublicitaria = service.BuscarPorId(id);
+		
 		
 		  if(id > 0) {
 			  campaniapublicitaria = service.listarId(id);
@@ -140,13 +167,17 @@ public class Controlador {
 
 	
 		model.addAttribute("campaniapublicitaria", new CampaniaPublicitaria());
-		return "ListaDeCampaniaUsuario";
+		return "usuario/ListaDeCampaniaUsuario";
 		
 	}
 	
 	/*
 	 * Este metodo busca por el id del usuario todas las campañas que tenga
 	 * vinculadas o que allá creado
+	 */
+	/**
+	 * Buscar en toda la lista el idusuario que sea igual al indicado
+	 * para luego agregarlos a una lista, para mostrarlos en una vista
 	 */
 	@GetMapping("/idusuario/")
 	public String BuscarPorIdusuario(@RequestParam String idusuario,Model model,@ModelAttribute("campaniapublicitaria") CampaniaPublicitaria campaniaPublicitaria ) {
@@ -155,9 +186,20 @@ public class Controlador {
 		return "usuario/ListaDeCampaniaUsuario";
 	}
 	
+	
+	@GetMapping("/inicioAdmin")
+	//llama
+	public String inicioAdmin(Model model) {
+		
+		return "inicioAdmin";
+	}
+	
+	
+	
 		
 	/**
-	 * Este metodo elimina una campaña por su id 
+	 * Busca el id en la lista para luego para luego tomar el objeto e
+	 * elimina una campaña por su id 
 	 */
 		@GetMapping("/eliminarcampania/{id}")
 	public String delete(Model model, @PathVariable int id) {
@@ -166,10 +208,27 @@ public class Controlador {
 	}
 	
 		
-	/*carteles publicitarios*/
+	/*Desde Esta parte comienzan las clases de los carteles publicitarios*/
 	@Autowired
 	private ICartelPublicitarioService servicecartel;
 	
+	/**
+	 * Toma los datos de la bd 
+	 * y crea una lista  de tipo CartelPublicitario para luego  mostrarlos en una vista
+	 * llamada listacartelesAdmin
+	 */
+	
+	@GetMapping("/listacartelesAdmin")
+	public String listarcartelesadmin(Model model) {
+		List<CartelPublicitario> cartelpublicitarios = servicecartel.listarcarteles();
+		model.addAttribute("cartelpublicitarios", cartelpublicitarios);
+		return "carteles/listacartelesAdmin";
+	} 
+	/**
+	 * Toma los datos de la bd 
+	 * y crea una lista  de tipo CartelPublicitario para luego  mostrarlos en una vista
+	 * llamada listacarteles
+	 */
 	@GetMapping("/listacarteles")
 	public String listarcarteles(Model model) {
 		List<CartelPublicitario> cartelpublicitarios = servicecartel.listarcarteles();
@@ -177,12 +236,24 @@ public class Controlador {
 		return "carteles/listacarteles";
 	} 
 	
+	/**
+	 * llama a la vista FormularioAgregarCartel.html
+	 * y la muestra.
+	 * ademas le entrega a la vista un texto"Agregar Cartel Publicitario" y un tipo Ojebto CartelPublicitario para luego Usarlo
+	 */
 	@GetMapping("/nuevocartel")
 	public String nuevocartel(Model model) {
+		model.addAttribute("titulo","Agregar Cartel Publicitario");
 		model.addAttribute("cartelpublicitario", new CartelPublicitario());
-		return "FormularioAgregarCartel";
+		return "carteles/FormularioAgregarCartel";
 	}
 	
+	/**
+	 * Esta Clase guarda el formulario de cartel
+	 * Ademas guarda las imagenes
+	 *
+	 * @return
+	 */
 	@PostMapping("/savecartel")
 	public String savecartel(@Validated CartelPublicitario c, Model model, @RequestParam("file") MultipartFile image, @RequestParam("document") MultipartFile documentos,RedirectAttributes atrAttributes) {
 		
@@ -210,15 +281,22 @@ public class Controlador {
 			}
 		}
 		servicecartel.savecartel(c);
-		return "redirect:/listarcarteles";
+		return "redirect:/listacartelesAdmin";
 	}
 	
+	/**
+	 * Buscar en la lista de carteles por el id entregado
+	 * luego entrega el id con el objeto asociado
+	 * y manda los datos a una vista para cargarlos 
+	 */
 	@GetMapping("modificarcartel/{id}")
 	public String editarcartel(@PathVariable int id,Model model) {
 		CartelPublicitario cartelpublicitario=servicecartel.listarIdcartel(id);
+		
+		model.addAttribute("titulo","Modificar-Cartel  "+ cartelpublicitario.getId());
 		model.addAttribute("cartelpublicitario",cartelpublicitario);
 		
-		return"FormularioAgregarCartel";
+		return"carteles/FormularioAgregarCartel";
 	}
 	/*lista por idusuario de carteles ingresados*/
 	@PostMapping("/listacarteles_usuario/{idusuario}")
@@ -231,10 +309,13 @@ public class Controlador {
 	}
 	
 	
+	/**
+	 * Buscar en la lista de carteles el id entregado y luego de encontralo elimina todo el objeto o la fila de la bd
+	 */
 	@GetMapping("/eliminarcartel/{id}")
 	public String deletecartel(Model del,@PathVariable int id) {
 		servicecartel.deletecartel(id);
-		return"redirect:/listarcartel";
+		return"redirect:/listacartelesAdmin";
 	}
 	@GetMapping("/agregarCartel")
 	//llama
@@ -243,10 +324,39 @@ public class Controlador {
 		return "carteles/FormularioAgregarCartel";
 	}
 	
+	/**
+	 * iniciamos un objeto en null
+	 * luego preguntamos el id entregado es mayor a 0 si es asi 
+	 * buscamos en la lista de carteles el id y si lo encuentra por carga en el objeto vacio iniciado anteriormente
+	 * 
+	 */
+	@GetMapping("detallecartel/{id}")
+	public String detalleCartel(@PathVariable("id") int id,Model model,RedirectAttributes attribute) {
+		
+		 CartelPublicitario cartelPublicitario = null;
+		
+		
+		  if(id > 0) {
+			  cartelPublicitario = servicecartel.listarIdcartel(id);
+		  
+		 if(cartelPublicitario==null) {
+			 attribute.addFlashAttribute("error","El ID del la Cartel no existe");
+			 return "DetalleCampania";
+		 }
+		  }else {
+			  attribute.addFlashAttribute("error","error con el id del cartel");
+			  return "DetalleCampania";
+		  }
+		model.addAttribute("titulo","Detalle de  "+ cartelPublicitario.getTitulo());
+		  
+		model.addAttribute("cartelpublicitario", cartelPublicitario);
+		return"carteles/DetalleCartel";
+	}
+	
 	@GetMapping("/index")
 	//llama de inicio 
 	public String incio(Model model) {
-		//model.addAttribute("cartelpublicitario", new CartelPublicitario());
+		
 		return "inicio";
 	}
 }
--- src/main/java/cl/christian/demo/modelo/CartelPublicitario.java
+++ src/main/java/cl/christian/demo/modelo/CartelPublicitario.java
@@ -22,11 +22,15 @@ public class CartelPublicitario {
 	private String correo;
 	private String nombre;
 	private String apellido;
+	private String detalle;
+	private int precio;
 	private int idusuario;
 	
+
+
 	public CartelPublicitario(int id, String titulo, String direccion, String region, String comuna,
 			String documentacion, String image, int nrocontacto, String correo, String nombre, String apellido,
-			int idusuario) {
+			String detalle, int precio, int idusuario) {
 		super();
 		this.id = id;
 		this.titulo = titulo;
@@ -39,104 +43,180 @@ public class CartelPublicitario {
 		this.correo = correo;
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.detalle = detalle;
+		this.precio = precio;
 		this.idusuario = idusuario;
 	}
+
+
 
 	public CartelPublicitario() {
 		super();
 	}
 
+
+
 	public int getId() {
 		return id;
 	}
+
+
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
+
+
 	public String getTitulo() {
 		return titulo;
 	}
+
+
 
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
 
+
+
 	public String getDireccion() {
 		return direccion;
 	}
+
+
 
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
 
+
+
 	public String getRegion() {
 		return region;
 	}
+
+
 
 	public void setRegion(String region) {
 		this.region = region;
 	}
 
+
+
 	public String getComuna() {
 		return comuna;
 	}
+
+
 
 	public void setComuna(String comuna) {
 		this.comuna = comuna;
 	}
 
+
+
 	public String getDocumentacion() {
 		return documentacion;
 	}
+
+
 
 	public void setDocumentacion(String documentacion) {
 		this.documentacion = documentacion;
 	}
 
+
+
 	public String getImage() {
 		return image;
 	}
+
+
 
 	public void setImage(String image) {
 		this.image = image;
 	}
 
+
+
 	public int getNrocontacto() {
 		return nrocontacto;
 	}
+
+
 
 	public void setNrocontacto(int nrocontacto) {
 		this.nrocontacto = nrocontacto;
 	}
 
+
+
 	public String getCorreo() {
 		return correo;
 	}
+
+
 
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
 
+
+
 	public String getNombre() {
 		return nombre;
 	}
+
+
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+
+
 	public String getApellido() {
 		return apellido;
 	}
+
+
 
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
 
+
+
+	public String getDetalle() {
+		return detalle;
+	}
+
+
+
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+
+
+
+	public int getPrecio() {
+		return precio;
+	}
+
+
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+
+
 	public int getIdusuario() {
 		return idusuario;
 	}
+
+
 
 	public void setIdusuario(int idusuario) {
 		this.idusuario = idusuario;
